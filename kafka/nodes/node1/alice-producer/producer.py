8c64d059eb6c51b72741c7b7685206bac13344be
AsiSkarp <asiskarp@gmail.com>
2022-11-14 10:22:17 +0100

    filepath fuckery

--- kafka/nodes/node1/alice-producer/producer.py
+++ kafka/nodes/node1/alice-producer/producer.py
@@ -7,7 +7,7 @@ uz_path = "/app/test_data/2015-01-01-15.json"
 
 # print(os.listdir(uz_path))
 
-with open(uz_path) as f:
+with open(uz_path, encoding="utf-8") as f:
     producer = KafkaProducer(bootstrap_servers="bddst-g04-Node1.uvm.sdu.dk:9092")
     for line in f:
         producer.send("alice-test", json.loads(line).encode("utf-8"))
