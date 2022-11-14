a9553fe049a0d51a83d97d9b376002d8e812adc2
AsiSkarp <asiskarp@gmail.com>
2022-11-14 09:38:30 +0100

    Add kafka-producer

--- kafka/nodes/node1/alice-producer/producer.py
+++ kafka/nodes/node1/alice-producer/producer.py
@@ -1,11 +1,14 @@
-from hdfs import InsecureClient
+import json
 from kafka import KafkaProducer
 
-client = InsecureClient('http://namenode:9870', user='root')
+with open("../../../../../test_data/2015-01-01-15.json.txt") as f:
+    producer = KafkaProducer(bootstrap_servers="bddst-g04-Node1.uvm.sdu.dk:9092")
+    for line in f:
+        producer.send("alice-test", json.loads(line).encode("utf-8"))
 
-with client.read('/alice-in-wonderland.txt') as reader:
-    text = reader.read().splitlines()
-    producer = KafkaProducer(bootstrap_servers='kafka:9092')
-    for line in text:
-        producer.send('alice-test', line.encode('utf-8'))
 
+# with client.read("../../../../../test_data/2015-01-01-15.json.txt") as reader:
+#    text = reader.read().splitlines()
+#    producer = KafkaProducer(bootstrap_servers="bddst-g04-Node1.uvm.sdu.dk:9092")
+#    for line in text:
+#        producer.send("alice-test", line.encode("utf-8"))
