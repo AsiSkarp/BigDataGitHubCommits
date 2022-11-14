ba21bcbb128202e1d934a8d4486a43edf624d4f8
AsiSkarp <asiskarp@gmail.com>
2022-11-14 10:01:54 +0100

    File path

--- kafka/nodes/node1/alice-producer/producer.py
+++ kafka/nodes/node1/alice-producer/producer.py
@@ -1,14 +1,13 @@
 import json
 from kafka import KafkaProducer
 
-with open("../../../../test_data/2015-01-01-15.json.txt") as f:
+uz_path = "../../../../../test_data/2015-01-01-15.json.txt"
+
+os.listdir(uz_path)
+
+"""
+with open(uz_path) as f:
     producer = KafkaProducer(bootstrap_servers="bddst-g04-Node1.uvm.sdu.dk:9092")
     for line in f:
         producer.send("alice-test", json.loads(line).encode("utf-8"))
-
-
-# with client.read("../../../../../test_data/2015-01-01-15.json.txt") as reader:
-#    text = reader.read().splitlines()
-#    producer = KafkaProducer(bootstrap_servers="bddst-g04-Node1.uvm.sdu.dk:9092")
-#    for line in text:
-#        producer.send("alice-test", line.encode("utf-8"))
+"""
