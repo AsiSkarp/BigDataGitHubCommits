bb55b97d146eebac838eb9091972005138d6864b
AsiSkarp <asiskarp@gmail.com>
2022-11-14 10:21:13 +0100

    filepath fuckery

--- kafka/nodes/node1/alice-producer/producer.py
+++ kafka/nodes/node1/alice-producer/producer.py
@@ -2,14 +2,12 @@ import os
 import json
 from kafka import KafkaProducer
 
-# "../../../../../test_data/2015-01-01-15.json.txt"
-uz_path = "/app/test_data/"
+# "../../../../../test_data/2015-01-01-15.json"
+uz_path = "/app/test_data/2015-01-01-15.json"
 
-print(os.listdir(uz_path))
+# print(os.listdir(uz_path))
 
-"""
 with open(uz_path) as f:
     producer = KafkaProducer(bootstrap_servers="bddst-g04-Node1.uvm.sdu.dk:9092")
     for line in f:
         producer.send("alice-test", json.loads(line).encode("utf-8"))
-"""
