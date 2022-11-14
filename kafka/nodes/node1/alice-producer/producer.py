d36b88ed1b4422eef0cf35695d5aa28eac406a03
AsiSkarp <asiskarp@gmail.com>
2022-11-14 12:52:59 +0100

    Add fetch-producer script

--- kafka/nodes/node1/alice-producer/producer.py
+++ kafka/nodes/node1/alice-producer/producer.py
@@ -5,8 +5,6 @@ from kafka import KafkaProducer
 # "../../../../../test_data/2015-01-01-15.json"
 uz_path = "/app/test_data/2015-01-01-15.json"
 
-# print(os.listdir(uz_path))
-
 with open(uz_path, encoding="utf-8") as f:
     producer = KafkaProducer(bootstrap_servers="bddst-g04-Node1.uvm.sdu.dk:9092")
     for line in f:
